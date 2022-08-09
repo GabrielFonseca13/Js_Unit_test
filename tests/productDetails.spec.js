8123326169d44cfc792e972e0c91dafe1ad745ac
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-08 21:10:05 -0300

    resolução do requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,14 +30,36 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+  // it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+  //   fail('Teste vazio!');
+
+    const resgataFuncao = productDetails('Alcool gel', 'Máscara');
+
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    it('Teste se productDetails é uma função', () => {
+      expect(typeof productDetails).toBe('function');
+    })
     // Teste se o retorno da função é um array.
+    it('Teste se o retorno da função é um array.', () => {
+      expect(Array.isArray(productDetails())).toBeTruthy();
+    })
     // Teste se o array retornado pela função contém dois itens dentro.
+    it('Teste se o array retornado pela função contém dois itens dentro.', () => {
+      expect(resgataFuncao.length).toBe(2);
+    })
     // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    it('Teste se os dois itens dentro do array retornado pela função são objetos.', () => {
+    expect(typeof resgataFuncao[0]).toBe('object');
+    expect(typeof resgataFuncao[1]).toBe('object');
+    })
+    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.]
+    it('Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.', () => {      
+    expect(resgataFuncao[0]).not.toEqual(resgataFuncao[1]);
+    })
     // Teste se os dois productIds terminam com 123.
+  //   it('Teste se os dois productIds terminam com 123.', () => {
+  //     expect(productDetails('Alcool gel', 'Máscara'))[0].toContain('123')
+  //   })
   });
-});
+
